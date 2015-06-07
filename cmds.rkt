2c9fadf5a971441530b97d754223e8cdf6558b87
Matt Jadud <matt@jadud.com>
2015-06-07 14:45:15 -0400

    Round trip with possible compilation.
    Ported over the syscall portion of the old server, as it was
    not so bad. Have a successful round trip, with call/response
    going cleanly between client and server. Looks like all the
    requisite information is in the configs...

---
+++ cmds.rkt
@@ -0,0 +1,153 @@
+;; The MIT License (MIT)
+;; 
+;; Copyright (c) 2015 Matthew C. Jadud
+;; 
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;; 
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;; 
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
+
+#lang racket
+(require "util.rkt"
+         "debug.rkt"
+         "syscalls.rkt"
+         "config.rkt")
+
+(provide compile
+         binhex
+         output-exists?
+         exe-in-session 
+         compile-cmd
+         compile
+         plink
+         plinker-cmd
+         binhex-cmd)
+
+;; RUNNING COMMANDS
+;; We'll define commands as S-expressions. The language
+;; looks like
+;; (cmd -flag0 (-flag1 value1) (= -p1 value2))
+;; which becomes
+;; "cmd -flag0 -flag1 value1 -p1=value2"
+;; Note we don't insert hyphens, but we make sure
+;; spaces come out right.
+
+
+     
+
+(define (compile session-dir cmd)
+  (parameterize ([current-directory (build-path (conf-get 'temp-dir) session-dir)])
+    (unless (directory-exists? (current-directory))
+      (make-directory* (current-directory)))
+    
+    (debug 'COMPILE "Current directory: ~a~n" (current-directory))
+    (debug 'COMPILE "****~n~a~n****~n" cmd)
+           
+    (let-values ([(stdout stdin pid stderr control)
+                  (apply values (process cmd))])
+      (define result (make-parameter 'UNKNOWN))
+      
+      (let loop ([status (control 'status)])
+        (case status
+          [(running) (sleep 1) (loop (control 'status))]
+          [(done-ok) 
+           ;; FIXME Some kind of response
+           'OK
+           ]
+          [(done-error)
+           (let ([err-msg (read-all stdout)]
+                 [details (read-all stderr)])
+             (close-input-port stdout)
+             (close-input-port stderr)
+             (close-output-port stdin)
+             (control 'kill)
+             ;; FIXME Build an error response
+             'ERROR
+             )]))
+      
+      (result))))
+    
+;; NEED TO PARAMETERIZE
+#|
+ avr-occbuild --program fastblink.occ 
+--search /home/jupiter/git/kroc/tvm/arduino/occam/include/ 
+--search /home/jupiter/git/kroc/tvm/arduino/occam/include/arch/common/ 
+--search /home/jupiter/git/kroc/tvm/arduino/occam/include/arch/m328p/ -D F.CPU=16000 
+--search /home/jupiter/git/kroc/tvm/arduino/occam/include/platforms/arduino
+|#
+
+(define (compile-cmd config main)
+  (system-call
+   (conf-get "occbuild")
+   `(--search ,(conf-get "include")
+              --search 
+              ,(build-path (conf-get "include") "arch" "common")
+              --search 
+              ,(build-path (conf-get "include") "arch" (hash-ref config "cpu" "m328"))
+              --search 
+              ,(build-path (conf-get "include") "platforms" 
+                           (hash-ref config "platform" "arduino"))
+              -D ,(format "F.CPU=~a" (hash-ref config 'mhz "16000000"))
+              ;; --program needs to come last
+              --program ,main)))
+
+(define (output-exists? session-dir names ext)
+  (parameterize ([current-directory (build-path (conf-get 'temp-dir) session-dir)])
+    (file-exists? (hash-ref names ext))))
+
+(define (plink config session-id names)
+  (define result 
+    (make-parameter 
+     (exe-in-session 
+      config
+      session-id 
+      (plinker-cmd config 
+                   (hash-ref names 'tce)
+                   (hash-ref names 'tbc)))))
+
+  (cond
+    [(zero? (result)) 
+     ;; FIXME Some kind of response
+     'OK
+     ]
+    [else (error)]))
+  
+
+(define (plinker-cmd config tce tbc)
+  (system-call
+   (send (config) get-config 'LINKER)
+   `(-s -o ,tbc
+        ,(->string ((send (config) get-config 'occam-lib-path) 'forall))
+        ,tce)))
+
+(define (binhex config session-id names)
+  (define result
+    (make-parameter
+     (exe-in-session config session-id (binhex-cmd config names))))
+  (cond
+    [(zero? (result)) 
+     ;;FIXME Some kind of response
+     'OK
+     ]
+    [else (error)]))
+
+;;FIXME : Magic Number (bytecode location)
+(define (binhex-cmd config names)
+  (system-call
+   (send (config) get-config 'BINHEX)
+   `(,(number->string (hash-ref (send (config) get-config 'BOARD) 'start-address) 16)
+     ,(hash-ref names 'tbc) 
+     ,(hash-ref names 'hex))))
---
+++ syscalls.rkt
@@ -0,0 +1,89 @@
+;; The MIT License (MIT)
+;; 
+;; Copyright (c) 2015 Matthew C. Jadud
+;; 
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;; 
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;; 
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
+
+#lang racket
+
+(require "config.rkt"
+         "util.rkt"
+         )
+(provide system-call
+         exe-in-tempdir
+         exe-in-session
+         )
+
+;; CODE EXECUTION
+;;;;;;;;;;;;;;;;;;
+
+(define (exe-in-tempdir temp-dir cmd)
+  (parameterize ([current-directory temp-dir])
+    (system/exit-code cmd)))    
+
+;; Server side... FIXME
+(define (exe-in-session id cmd)
+  (parameterize ([current-directory (conf-get 'session-dir)])
+    (system/exit-code cmd)))
+
+(define (system-call prog flags)
+  (format "~a ~a"
+          ;(build-bin-path prog)
+          prog
+          (render (parse flags))))
+
+
+(struct cmd (app args) #:transparent)
+(struct arg2 (flag value) #:transparent)
+(struct arg1 (flag) #:transparent)
+(struct set (param value) #:transparent)
+(struct nospace (flag value) #:transparent)
+
+(define (parse sexp)
+  (match sexp
+    [`(= ,rand1 ,rand2)
+     (set rand1 rand2)]
+    [`(nospace ,flag ,val)
+     (nospace flag val)]
+    [`(,command ,args ...)
+     (cmd command (map parse args))]
+    ;; FIXME: The list of length two is subsumed by
+    ;; the previous rule... unnecessary?
+    [`(,flag ,value)
+     (arg2 flag value)]
+    [flag/value
+     (arg1 flag/value)]))
+
+
+(define (render ast)
+  (match ast
+    [(struct cmd (command args))
+     (format "~a ~a" 
+             command
+             (apply string-append
+                    (list-intersperse (map render args) " ")))]
+    [(struct set (param value))
+     (format "~a=~a" param value)]
+    [(struct arg2 (flag value))
+     (format "~a ~a" flag value)]
+    [(struct arg1 (flag/value))
+     (format "~a" flag/value)]
+    [(struct nospace (flag value))
+     (format "~a~a" flag value)]
+    ))
