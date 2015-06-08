0b0f40ad8aaba35f3a81832bc4d51730f8b604a5
Matt Jadud <matt@jadud.com>
2015-06-08 12:08:59 -0400

    Now compiling and uploading.
    It turns out bad things happened to FTDI drivers on 10.9. Grr.
    
    This is not yet a cross-platform upload... plumb needs more work.
    That, or I need to go back to my IDE, and fix things.
    
    Either way, this will do the command line on Linux and OSX, I think. At least, it works for me on Linux and OSX. And, with two test programs. That's practically bullet-proof testing.
    
    Either way, it's a first step. Now we can compile and upload.

---
+++ constants.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(provide (all-defined-out))
+
+(define OK.COMPILE 'OK.COMPILE)
+(define OK.LINK 'OK.LINK)
+(define OK.BINHEX 'OK.BINHEX)
+(define OK.IHEXMERGE 'OK.IHEXMERGE)
+(define OK.AVRDUDE 'OK.AVRDUDE)
+
+(define ERR.COMPILE 'ERR.COMPILE)
+(define ERR.LINK 'ERR.LINK)
+(define ERR.BINHEX 'ERR.BINHEX)
+(define ERR.IHEXMERGE 'ERR.IHEXMERGE)
+(define ERR.AVRDUDE 'ERR.AVRDUDE)
